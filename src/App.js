c044c893a29482021daa2cbc2af7716cf5e980e1
Alan <a.corrales@sidi.org.br>
2020-06-17 22:55:33 -0300

    Adds simple navigation to shopping items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
