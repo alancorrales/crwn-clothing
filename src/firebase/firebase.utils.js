f5863c7294610355f7fee742f3503000e153de9e
Alan <a.corrales@sidi.org.br>
2020-07-01 01:05:56 -0300

    Adds user storage logic on login

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
     appId: "1:741047299472:web:9f5b88b95b8ef1e69bd715"
 };
 
+/* References may give snapshot objects (like documents and collections) */
+
+// Creates user if they don't exist in db
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
