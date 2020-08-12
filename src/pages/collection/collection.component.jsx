91ac3ce5fcf11b4f46ba3156408511dcfd3f6c7f
Alan <a.corrales@sidi.org.br>
2020-08-12 01:35:25 -0300

    Moves directory and shop data into its own reducer, creates respective selectors and updates corresponding components with new redux flow for directory/shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './collection.styles.scss';
+
+import { selectCollection } from '../../redux/shop/shop.selector';
+import { connect } from 'react-redux';
+import CollectionItem from './../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({ collection, match }) => {
+	const { title, items } = collection;
+	return (
+		<div className='collection-page'>
+			<h1 className='title'>{title}</h1>
+			<div className='items'>
+				{items.map((item) => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+const mapStateToProps = (state, ownProps) => ({
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from "redux-persist/lib/storage"; // localStorage
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', // From what point of the reducer do we want to start storing?
+    storage,
+    whitelist: [
+        'cart'
+    ] // What do we want to persist? Only cart, bc user's already being persisted by firebase
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
-})
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
